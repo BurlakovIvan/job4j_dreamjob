31cbb9625a6959dda66959485eb94a75ccdacc3a
BurlakovIvan <burlakovid@inbox.ru>
2022-09-10 22:04:24 +0300

    PostController.createPost. Создание вакансии

--- src/main/java/ru/job4j/dreamjob/controller/PostController.java
+++ src/main/java/ru/job4j/dreamjob/controller/PostController.java
@@ -3,10 +3,13 @@ package ru.job4j.dreamjob.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import ru.job4j.dreamjob.model.Candidate;
 import ru.job4j.dreamjob.model.Post;
 import ru.job4j.dreamjob.store.PostStore;
 
+import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDate;
 
 @Controller
@@ -30,5 +33,12 @@ public class PostController {
     public String addCandidate(Model model) {
         model.addAttribute("candidate", new Candidate(0, "Заполните ФИО", "Заполните описание", LocalDate.now()));
         return "addCandidate";
+    }
+
+    @PostMapping("/createPost")
+    public String createPost(@ModelAttribute Post post) {
+        post.setCreated(LocalDate.now());
+        store.add(post);
+        return "redirect:/posts";
     }
 }
