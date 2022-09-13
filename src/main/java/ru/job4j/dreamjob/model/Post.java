1bbb6826add00d1a2b2908471900b24726b95bbd
BurlakovIvan <burlakovid@inbox.ru>
2022-09-13 22:00:17 +0300

    Формы. Поля ввода

--- src/main/java/ru/job4j/dreamjob/model/Post.java
+++ src/main/java/ru/job4j/dreamjob/model/Post.java
@@ -1,15 +1,18 @@
 package ru.job4j.dreamjob.model;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.Objects;
 
-public class Post {
+public class Post implements Serializable {
     private int id;
     private String name;
 
     private String description;
 
     private LocalDate created;
+
+    private boolean visible;
 
     public Post() { }
 
@@ -50,6 +53,14 @@ public class Post {
 
     public void setCreated(LocalDate created) {
         this.created = created;
+    }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
     }
 
     @Override
